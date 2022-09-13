495cabf2ffdf1b3b71cab2ea75d384d04c183a00
duong13012002 <102996301+duong13012002@users.noreply.github.com>
2022-09-13 10:06:09 +0700

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+import { ToggleComponent } from './toggle.component';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,20 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  @ViewChild(ToggleComponent,{static:true}) toggleComp: ToggleComponent
+  @ViewChild('nameInput',{static:true}) nameInput: ElementRef<HTMLInputElement>
+
   name = 'Angular ' + VERSION.major;
+  isChecked = true;
+
+  ngOnInit(){
+    setTimeout(()=>{
+      this.nameInput.nativeElement.focus()
+    },3000)
+    console.log('onInit',this.toggleComp)
+  }
+
+  ngAfterViewInit(){
+    console.log(this.toggleComp)
+  }
 }
